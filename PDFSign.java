d3fc9e2c917375beee2469bf75b369d14256691f
koukanon <164488212+koukanon@users.noreply.github.com>
2024-06-11 11:55:56 +0800

    Add files via upload

--- PDFSign.java
+++ PDFSign.java
@@ -1,4 +1,35 @@
+/*
+ * 
+ * Copyright (C) 2024 BSMI/kangdainfo 
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License along with this program.If not, see <https://www.gnu.org/licenses/>.
 
+
+提供使用JAVA的程式,使用itextpdf 5.1.3進行PDF的加簽功能, 
+itextpdf套件為HomePage http://www.itextpdf.com/ 
+注意Licenses為GNU Affero General Public License v3 
+http://www.fsf.org/licensing/licenses/agpl-3.0.html
+
+版權所有 (C) 2024 經濟部標準檢驗局/康大資訊股份有限公司
+本程式是自由軟體：您可以基於自由軟體基金會發佈的
+GNU Affero通用公共許可證的條款下重新分發和/或修改它，
+或者本許可證第三版或者（由您選擇）任何後續版本。    
+
+分發本程式是希望它能派上用場，但沒有任何擔保，
+甚至也沒有對其適銷性或特定目的適用性的默示擔保。
+更多細節請參見“GNU Affero通用公共許可證”。    
+
+您應該已收到本程式隨附的GNU Affero通用公共許可證的副本。
+如未收到，請參見：http://www.gnu.org/licenses/ 。
+
+EMAIL   Kh.Li@bsmi.gov.tw
+
+README.md
+	
+ * 
+ * 
+ */
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.security.KeyStore;
